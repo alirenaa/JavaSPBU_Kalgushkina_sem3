1dc9789acd46f6cb476f1185a321a392412ce46d
alirenaa <nika.meco@gmail.com>
2021-11-30 00:55:44 +0300

    Изменили решение еще раз

---
+++ src/IntroTask3Digits.java
@@ -0,0 +1,7 @@
+public class IntroTask3Digits {
+    public static void main(int x) {
+        if (x <= 999 && x >= 100)
+            System.out.println("ровно 3");
+        else System.out.println("не ровно 3");
+    }
+}
