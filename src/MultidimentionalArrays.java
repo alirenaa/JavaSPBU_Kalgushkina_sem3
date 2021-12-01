6f4d1b38c4ef16e9c5440935a00b0173f3bda8c6
alirenaa <nika.meco@gmail.com>
2021-12-01 15:52:28 +0300

    Добавлены задачи на многомерные массивы

---
+++ src/MultidimentionalArrays.java
@@ -0,0 +1,46 @@
+import java.util.Arrays;
+
+public class MultidimentionalArrays {
+    public static void main(String args[]) {
+        int[][] a = new int[][]{
+                {10, 20, 30},
+                {40, 50, 60, 70, 80, 90},
+                {100, 200}
+        };
+        System.out.println("Задача printTable");
+        printTable(a);
+        System.out.println("Задача printTableAligned");
+        printTableAligned(a);
+    }
+
+    public static void printTable(int[][] a){
+        for (int i = 0; i < a.length; i++) {
+            for (int j = 0; j < a[i].length; j++) {
+                System.out.print(a[i][j] + " ");
+            }
+            System.out.print("\n");
+        }
+    }
+
+    public static void printTableAligned(int[][] a){
+        int max_len = a[0].length; // длина самой длинной строки
+        for (int i = 0; i < a.length; i++) {
+            if (max_len < a[i].length) max_len = a[i].length;
+        }
+        int[] max_numbers = new int[max_len]; // массив максимальных чисел в каждом столбце
+        for (int i = 0; i < a.length; i++) {
+            for (int j = 0; j < a[i].length; j++) {
+                if (max_numbers[j] < a[i][j]) max_numbers[j] = a[i][j];
+            }
+        }
+
+        for (int i = 0; i < a.length; i++) {
+            for (int j = 0; j < a[i].length; j++) {
+                int x = String.valueOf(Math.abs(max_numbers[j])).length() - String.valueOf(Math.abs(a[i][j])).length();
+                System.out.print(" ".repeat(x) + a[i][j] + "  ");
+            }
+            System.out.print("\n");
+        }
+    }
+}
+
