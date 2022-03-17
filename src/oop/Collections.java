904aee6ac2136af3f473f474691b8456ba7a0a51
alirenaa <90378075+alirenaa@users.noreply.github.com>
2022-03-17 21:09:43 +0300

    collections

--- src/oop/Collections.java
+++ src/oop/Collections.java
@@ -1,13 +1,50 @@
 package oop;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Path;
+import java.util.*;
 import java.util.stream.Stream;
 
 public class Collections {
 
+    public static void main(String[] args) throws Exception {
+        System.out.println( count(3));
+        List<String> list1 = List.of("abc", "xyz", "ooo");
+        printList(list1);
+        List<String> list2 = List.of("ab", "xyz", "oooo");
+        printListWithIndices(list2);
+
+        List<String> list3 = List.of("aaa", "bbb", "ccc");
+        List<String> list4 = List.of("xxx", "yyy", "zzz");
+        List<String> list3plusList4 = concatenateLists(list3, list4);
+        System.out.println(list3plusList4);
+        System.out.println(list3);
+        System.out.println(list4);
+
+        List<String> list5 = new ArrayList<>(List.of("first", "middle", "last"));
+        List<String> list5rev = reverseList(list5);
+        System.out.println("list5rev = " + list5rev + ", but list5 = " + list5);
+
+        reverseListInPlace(list5);
+        System.out.println("list5 = " + list5);
+
+        List<Integer> ints = new ArrayList<>(List.of(11, 22, 33, 55, 66, 88, 100, 3, 4));
+        System.out.println(filterEvenIndices(ints));
+        System.out.println(filterEven(ints));
+
+        System.out.println(" ============= mutable lists =================== ");
+
+        List<Integer> mutableInts = new ArrayList<>(ints);
+        mutableFilterEvenIndices(mutableInts);
+        System.out.println("ints no even indices = " + mutableInts);
+
+        mutableInts = new ArrayList<>(ints);
+        mutableFilterEven(mutableInts);
+        System.out.println("ints without even = " + mutableInts);
+    }
+
     private static List<Integer> count(int n) {
-        ArrayList<Integer> arr = new ArrayList<>();
+        List<Integer> arr = new ArrayList<>();
         for (int i = 1; i <= n; i++) {
             arr.add(i);
         }
@@ -27,13 +64,13 @@ public class Collections {
     }
 
     public static List<String> concatenateLists(List<String> list1, List<String> list2) {
-        ArrayList<String> res = new ArrayList<>(list1);
+        List<String> res = new ArrayList<>(list1);
         res.addAll(list2);
         return res;
     }
 
     public static List<String> reverseList(List<String> arr) {
-        ArrayList<String> res = new ArrayList<>();
+        List<String> res = new ArrayList<>();
         for (int i = arr.size() - 1; i >= 0; i--)
             res.add(arr.get(i));
         return res;
@@ -47,6 +84,42 @@ public class Collections {
         }
     }
 
+    private static List<Integer> filterEvenIndices(List<Integer> arr) {
+        List<Integer> res = new ArrayList<>();
+        for (int i = 0; i < arr.size(); i++)
+            if (i % 2 != 0)
+                res.add(arr.get(i));
+        return res;
+    }
 
+    private static List<Integer> filterEven(List<Integer> arr) {
+        List<Integer> res = new ArrayList<>();
+        for (int i: arr)
+            if (i % 2 != 0)
+                res.add(i);
+        return res;
+    }
+
+    private static void mutableFilterEvenIndices(List<Integer> arr) {
+        for (int i = 0; i < arr.size(); i++)
+            arr.remove(i);
+    }
+
+    private static void mutableFilterEven(List<Integer> arr) {
+        for (int i = 0; i < arr.size(); i++)
+            if (arr.get(i) % 2 == 0){
+                arr.remove(i);
+                i--;
+            }
+    }
+
+//    private static void doReadWordsInFile(String fileForReading, Set<String> res) throws Exception {
+//        Path txt = Path.of(fileForReading);
+//        try (Scanner in = new Scanner(txt, StandardCharsets.UTF_8)) {
+//            while (in.hasNext()) {
+//                res.add(in.next().toLowerCase());
+//            }
+//        }
+//    }
 
 }
